f291bec3e805fccf5a4ef3d47bf5c8a30f651543
d.tarasov <mrsndmn46@gmail.com>
2024-10-16 12:01:57 +0300

    init clusterisation script

---
+++ scripts/dataset_info.py
@@ -0,0 +1,11 @@
+from datasets import Dataset
+
+ds = Dataset.load_from_disk("data/segments.dataset")
+
+df = ds.to_pandas()
+
+print("ID's Values Counts")
+print(df['id'].value_counts())
+
+print("\nTotal count of segments")
+print(len(df))
