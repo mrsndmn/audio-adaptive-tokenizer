571c2a7537f157cfe3e4025315192f6a462a71f0
d.tarasov <mrsndmn46@gmail.com>
2024-10-21 11:29:09 +0000

    add test for audio tokenization bug

--- src/aat/tokenizer_test.py
+++ src/aat/tokenizer_test.py
@@ -3,6 +3,9 @@ import numpy as np
 from .audio import AudioWaveform
 from .tokenizer import AdaptiveAudioAmplitudeTokenizer
 
+import torchaudio
+import torch
+
 def _silence_waveform(tokenizer):
     sampling_rate = tokenizer.sampling_rate
     seconds_duration = 2
@@ -29,3 +32,21 @@ def test_tokenizer():
 
     sum_frames = sum(x.waveform.shape[-1] for x in segments)
     assert sum_frames == wf_sr.waveform.shape[-1]
+
+def test_tokenizer_real_audio():
+    tokenizer = AdaptiveAudioAmplitudeTokenizer()
+
+    waveform, sr = torchaudio.load("test_data/wav/speech_man.wav")
+    wf_sr = AudioWaveform(waveform[0], sr)
+
+    segments = tokenizer.tokenize(wf_sr)
+    segments_frames = [ x.waveform.shape[0] for x in segments]
+    print(len(segments), segments_frames)
+
+    assert min(segments_frames) != max(segments_frames) # check not all segments are equal
+
+    assert min(segments_frames) >= tokenizer.min_segment_frames
+    assert min(segments_frames) < tokenizer.max_segment_frames * 0.5
+
+    assert max(segments_frames) <= tokenizer.max_segment_frames
+    assert max(segments_frames) > tokenizer.min_segment_frames * 2
